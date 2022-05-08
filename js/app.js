c3491917969adfc6bda5a2e7ea2ebd1b6075c728
Gaurav Gupta <itsmegaurav60@gmail.com>
2022-05-08 17:33:44 +0530

    code changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var app = {
     displayKnowMoreSection: function(event){
         let element  = event.target;
         let parentNode = $(element).parents(".product");
-        console.log(parentNode.find(".section-container"))
         parentNode.find(".section-container")[0].classList.add("hidden");
         parentNode.find(".know-more-wrapper")[0].classList.remove("hidden");
 
@@ -12,10 +11,29 @@ var app = {
             parentNode[0].classList.add("h-extra-five");
         }
     },
+    handleHamburgerMenu: function(event){
+        let openIcon = $(".hamb-icon.open-icon")[0];
+        let closeIcon = $(".hamb-icon.close-icon")[0];
+        let hamburgerMenu = $(".hamburger-menu")[0];
+        let currentElement = event.target;
+        console.log(currentElement)
+        if($(currentElement)[0].classList.contains("open-icon")){
+            openIcon.classList.remove("active");
+            closeIcon.classList.add("active");
+            hamburgerMenu.classList.add("active");
+        } else {
+            closeIcon.classList.remove("active");
+            openIcon.classList.add("active");
+            hamburgerMenu.classList.remove("active");
+        }
+    },
     init: function() {
         let that = this;
         $(".action-link-product-cat").on("click", function(event){
             that.displayKnowMoreSection(event);
+        });
+        $(".hamb-icon").on("click", function(event){
+            that.handleHamburgerMenu(event);
         });
     }
 }
